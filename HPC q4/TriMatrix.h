7c8ce5417a9bf61435f205ccfe28d2c02cc6d613
Ernest Kan <ek96122@gmail.com>
2016-03-24 14:45:17 +0000

    start

---
+++ HPC q4/TriMatrix.h
@@ -0,0 +1,13 @@
+//
+//  TriMatrix.h
+//  HPC q4
+//
+//  Created by Ernest on 24/3/2016.
+//  Copyright © 2016 Kan Tsz Hang. All rights reserved.
+//
+
+#ifndef TriMatrix_h
+#define TriMatrix_h
+
+
+#endif /* TriMatrix_h */
