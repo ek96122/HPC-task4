33089b32b891ab34fd459346358f021784b17f75
Ernest Kan <ek96122@gmail.com>
2016-03-24 19:38:43 +0000

    1st ready to compile script (trial)

--- HPC q4/TriMatrix.h
+++ HPC q4/TriMatrix.h
@@ -8,6 +8,112 @@
 
 #ifndef TriMatrix_h
 #define TriMatrix_h
+#include <vector>
+#include <iostream>
+#include <Accelerate/Accelerate.h>
+
+
+using namespace std;
+
+class TriMatrix{
+    
+private:
+    double *diag;
+    double *u_diag;
+    double *l_diag;
+    double *A;
+    int length;
+    
+public:
+  
+    TriMatrix(int Nx, double nu){
+        l_diag = new double[Nx];
+        diag = new double[Nx+1];
+        u_diag = new double[Nx];
+        
+        for (int i = 0; i < Nx + 1; i++){
+            diag[i] = 1-2*nu;
+        }
+        
+        for (int i = 0; i < Nx; i++){
+            l_diag[i] = nu;
+            u_diag[i] = nu;
+        }
+        diag[0] = 1;
+        diag[Nx] = 1;
+        l_diag[Nx-1] = 0;
+        u_diag[0] = 0;
+        length = Nx + 1;
+        
+    }
+    
+    void convertMatrix(){
+        A = new double [length*length];
+        
+        for (int i=0; i<length; i++) {
+            A[i]=0;
+        }
+        
+        for (int i=0; i<length; i++){
+            A[i+i*length]=diag[i];
+        }
+    
+        for (int i=0; i< length-1; i++){
+            A[i+i*length+1]=l_diag[i];
+        }
+    
+        for (int i=0; i<length-1; i++){
+            A[i+(i+1)*length]=u_diag[i];
+        }
+    }
+    
+        
+    
+    
+    //Create a function which can perform matrix multiplication for the tridiagonal matrix with any Nx1 matrix, where N=number of rows of the tridiagonal matrix
+        double *blasmultiply(double *x){
+        int alpha = 1;
+        int beta = 0;
+        double *r;
+        
+        r = new double[length];
+    
+        cblas_dgemv(CblasColMajor, CblasNoTrans, length, length, alpha, A, length, x, 1, beta, r, 1);
+        
+      
+        return r;
+    }
+    
+    
+    double *inverse(double *x){
+        int nrhs = 1;
+        int info;
+        double *dl, *d, *du;
+        dl = new double[length-1];
+        d = new double[length];
+        du = new double[length-1];
+        copy(l_diag, l_diag + length-1, dl);
+        copy(diag, diag + length, d);
+        copy(u_diag, u_diag + length-1, du);
+        
+        dgtsv_(&length, &nrhs, dl, d, du, x, &length, &info);
+        
+        return x;
+        
+    }
+    
+    
+        
+        //cout << endl << "U2: " << endl;
+        //for(int i=0; i<D; i++) cout << (U2)[i] << ", ";
+        //cout<<endl;
+    
+        
+        
+
+    
+};
+
 
 
 #endif /* TriMatrix_h */
--- HPC q4/main.cpp
+++ HPC q4/main.cpp
@@ -7,9 +7,100 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "TriMatrix.h"
+#include <Accelerate/Accelerate.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+    double L, T, alpha, dt, theta;
+    int Nx;
+    
+    
+    // get user input
+    {using namespace std;
+        cout << "L: ";
+        cin >> L;
+        
+        cout << "T: ";
+        cin >> T;
+        
+        cout << "alpha: ";
+        cin >> alpha;
+        
+        cout << "Nx: ";
+        cin >> Nx;
+        
+        cout << "dt: ";
+        cin >> dt;
+        
+        cout<< "theta (0 for forward Euler)/(0.5 for Crank-Nicolson)/(1 for backward Euler) :";
+        cin >> theta;
+    }
+    
+    double dx = L/Nx;
+    double nu = alpha*dt/dx/dx;
+    double gamma0 = 0;
+    double gamma1 = 0;
+    
+    
+    //Defining all BASIC vectors used in the programme
+    
+    double x[Nx+1];
+    double u0[Nx+1];
+    double *u1;
+    u1 = new double[Nx+1];
+    double *u2;
+    u2 = new double [Nx+1];
+    
+    //Defining x-vector
+    for (int i=0;i<Nx+1;i++) {
+        x[i]=dx*i; //equallly spaced elements in x-vector
+    }
+    
+    //Defining a vector, which uses the initial condition u0, to hold the solution
+    for (int i=0; i<Nx+1; i++) {
+        u0[i]=x[i]/(1-x[i]);
+    }
+    
+    //Transfer entries from u0 vector to the u1 vector created earlier
+    
+    u1[0]=gamma0;
+    u1[Nx]=gamma1;
+    
+    for (int i=1; i<Nx; i++) {
+        
+        u1[i] = u0[i];
+    }
+    
+    
+    
+    TriMatrix Left(Nx, theta*nu*-1);
+    TriMatrix Right(Nx, (1-theta)*nu);
+    
+    
+    
+    //forward Euler time integration via matrix multiplication
+    
+    for (double i=0; i<(T-dt); i+=dt){
+        
+        u2=Left.inverse(Right.blasmultiply(u1));
+        u1=u2;}
+    
+    cout<<endl;
+    cout <<"time-integration output" <<endl;
+    for (int i=0; i< Nx + 1; i++){
+        cout<<u1[i] << endl;
+    }
+
+    
+    
+    
+
+    
+    
     return 0;
+    
+    
 }
