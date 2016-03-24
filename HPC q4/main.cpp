0045abf6b491a0d955f4b412ebfa4d78e2ad343f
Ernest Kan <ek96122@gmail.com>
2016-03-24 14:42:28 +0000

    Initial Commit

---
+++ HPC q4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HPC q4
+//
+//  Created by Ernest on 24/3/2016.
+//  Copyright © 2016 Kan Tsz Hang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
